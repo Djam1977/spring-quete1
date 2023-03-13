25868aedb313e11b0a5e5c58fc4e673ca006ac1c
Djam1977 <117649014+Djam1977@users.noreply.github.com>
2023-03-13 14:14:45 +0100

    quete1

---
+++ src/main/java/com/wildcodesschool/srpingquete1/Srpingquete1Application.java
@@ -0,0 +1,13 @@
+package com.wildcodesschool.srpingquete1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srpingquete1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Srpingquete1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodesschool/srpingquete1/Srpingquete1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodesschool.srpingquete1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srpingquete1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
